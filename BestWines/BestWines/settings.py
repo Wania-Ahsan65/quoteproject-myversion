c07ef71ee2cd0a57233edf0f6bae61d5c698c11d
Muhammad Mohsin <120400500+mohsin233@users.noreply.github.com>
2025-06-24 16:25:52 +0100

    Update settings.py

--- BestWines/BestWines/settings.py
+++ BestWines/BestWines/settings.py
@@ -78,23 +78,13 @@ WSGI_APPLICATION = 'BestWines.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import dj_database_url
-import os
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
