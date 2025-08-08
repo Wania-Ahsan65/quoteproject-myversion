0635bd26304376ffd8004097226a82cc292d0585
Wania-Ahsan65 <waniaahsan30@gmail.com>
2025-08-08 20:37:13 +0500

    reduced spacing in product section

--- BestWines/products/views.py
+++ BestWines/products/views.py
@@ -116,6 +116,7 @@ def product_list_view(request):
             'size': main_info.size,
             'abv': main_info.abv,
             'country': main_info.country,
+            'cost_price': main_info.cost_price,
             'variants': [{'brand': variant.brand, 'cost_price': variant.cost_price} for variant in variants],
         })
     
